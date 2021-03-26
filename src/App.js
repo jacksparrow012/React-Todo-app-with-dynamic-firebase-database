bfbaf255835ffcf7ae1382afaf8e7e155b024c24
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-26 20:13:03 +0600

    todo list now moved to seperate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import './App.css';
-
+import Todo from "./Todo"
+import { Button, InputLabel, Input, FormControl } from '@material-ui/core'
 function App() {
   const [todos, setTodos] = useState(["Take dogs fo a walk", "Take the rubbish out"])
   const [input, setInput] = useState('')
@@ -13,13 +14,23 @@ function App() {
     <div className="App">
       <h2>Hello world</h2>
       <form>
-        <input value={input} onChange={(event) => setInput(event.target.value)} />
+        <FormControl>
+
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={(event) => setInput(event.target.value)} />
+        </FormControl>
+        {/* <TextField id="outlined-basic" label="Write Todo" variant="outlined" value={input} onChange={(event) => setInput(event.target.value)} /> */}
+        {/* <input value={input} onChange={(event) => setInput(event.target.value)} /> */}
         {/* <button onClick={() => setTodos(todos.concat(input))}>Add Todo</button> */}
-        <button type="submit" onClick={addTodo}>Add Todo</button>
+        {/* <button onClick={addTodo}>Add todo</button> */}
+        <Button disabled={!input} type="submit" variant="contained" color="primary" onClick={addTodo} >
+          Add Todo
+</Button>
+
       </form>
       <ul>
         {todos.map(todo => {
-          return <li>{todo}</li>
+          return <Todo text={todo} />
         })}
       </ul>
     </div>
---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { List, ListItem, ListItemText } from '@material-ui/core'
+const Todo = (props) => {
+    return (
+
+        <List>
+            <ListItem>
+                <ListItemText primary="Todo" secondary={props.text} />
+            </ListItem>
+        </List>
+
+    );
+};
+
+export default Todo;
