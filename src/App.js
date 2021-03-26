78bfaacc3a45d164698b3925d8b52a34feb17bdd
Nazrul islam <jacksparrow.alex@gmail.com>
2021-03-26 19:10:43 +0600

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(["Take dogs fo a walk", "Take the rubbish out"])
+  const [input, setInput] = useState('')
+  const addTodo = (e) => {
+    e.preventDefault()
+    setTodos([...todos, input])
+    setInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello world</h2>
+      <form>
+        <input value={input} onChange={(event) => setInput(event.target.value)} />
+        {/* <button onClick={() => setTodos(todos.concat(input))}>Add Todo</button> */}
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map(todo => {
+          return <li>{todo}</li>
+        })}
+      </ul>
     </div>
   );
 }
